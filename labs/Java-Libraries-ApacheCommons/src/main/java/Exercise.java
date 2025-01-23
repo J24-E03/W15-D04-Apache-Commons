fa1ce8a5dc12616e4552be5aaa08fab2f7501d64
Michael <michael@gmail.com>
2025-01-23 16:57:08 +0100

    tasks done

--- labs/Java-Libraries-ApacheCommons/src/main/java/Exercise.java
+++ labs/Java-Libraries-ApacheCommons/src/main/java/Exercise.java
@@ -1,64 +1,81 @@
+package main.java;
+
+import org.apache.commons.io.FileUtils;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.List;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 
 public class Exercise {
-  public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-    // Import library
-    if (checksum()) {
-      System.out.println("Successfully imported the Apache Commons IO library");
-    } else {
-      System.out.println("Wrong version of the library");
+        // Import library
+        if (checksum()) {
+            System.out.println("Successfully imported the Apache Commons IO library");
+        } else {
+            System.out.println("Wrong version of the library");
+        }
+
+        // Working with Streams
+        readWebPage("https://digitalcareerinstitute.org/");
+
+        // Working with files
+        readFile("LoremIpsum.txt");
+        copyFile("LoremIpsum.txt");
+
+        // Find files with extension .java in other project
+        findJavaFiles("./");
     }
 
-    // Working with Streams
-    readWebPage("https://digitalcareerinstitute.org/");
 
-    // Working with files
-    readFile("LoremIpsum.txt");
-    copyFile("LoremIpsum.txt");
+    private static boolean checksum() throws IOException {
+        // TODO: Add your code here
+        long checksum = FileUtils.checksumCRC32(new File(FileUtils.class.getProtectionDomain().getCodeSource().getLocation().getFile()));
+        return checksum == 2449403980L;
+    }
 
-    // Find files with extension .java in other project
-    findJavaFiles("./");
-  }
+    private static void readWebPage(String url) throws IOException, URISyntaxException {
+        System.out.println("\n\nReading " + url);
 
+        // TODO: Add your code here
+        URL link = new URI(url).toURL();
 
-  private static boolean checksum() throws IOException {
-    // TODO: Add your code here
+        HttpsURLConnection connection = (HttpsURLConnection) link.openConnection();
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-    return checksum == 2449403980L;
-  }
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
 
-  private static void readWebPage(String url) throws IOException {
-    System.out.println("\n\nReading " + url);
-    InputStream in = new URL(url).openStream();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println(response);
+    }
 
-    // TODO: Add your code here
-  }
+    private static void readFile(String fileName) throws IOException {
+        System.out.println("\n\nReading LoremIpsum.txt:");
 
-  private static void readFile(String fileName) throws IOException {
-    System.out.println("\n\nReading LoremIpsum.txt:");
-    // TODO: Add your code here
-  }
+        // TODO: Add your code here
+        System.out.println(FileUtils.readFileToString(new File("labs/Java-Libraries-ApacheCommons/src/main/resources/" + fileName), StandardCharsets.UTF_8));
+    }
 
-  private static void copyFile(String fileName) throws IOException, InterruptedException {
-    System.out.println("\n\nCreating a copy of LoremIpsum.txt in " + FileUtils.getTempDirectory());
-    // TODO: Add your code here
+    private static void copyFile(String fileName) throws IOException {
+        System.out.println("\n\nCreating a copy of LoremIpsum.txt in " + FileUtils.getTempDirectory());
 
-    Thread.sleep(150);
+        // TODO: Add your code here
+        FileUtils.copyFile(new File("labs/Java-Libraries-ApacheCommons/src/main/resources/" + fileName), new File(FileUtils.getTempDirectory() + "/" + fileName));
+    }
 
-    // TODO: Add your code here
-  }
-
-  private static void findJavaFiles(String relativePath) {
-    System.out.println("\n\nListing all java files in directory " + relativePath);
-  }
+    private static void findJavaFiles(String relativePath) {
+        System.out.println("\n\nListing all java files in directory " + relativePath);
+        System.out.println(FileUtils.listFiles(new File(relativePath), new String[]{"java"}, true));
+    }
 }
